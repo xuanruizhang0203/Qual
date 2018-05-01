4bdd71a426263c8972795a3269abf42c7c49999e
xuanruizhang0203 <31296560+xuanruizhang0203@users.noreply.github.com>
2018-05-01 18:49:58 -0400

    Add files via upload

---
+++ app/Qual.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text  } from "react-native";
+import {MainFeed, Home} from './components/screens';
+
+
+class Qual extends Component{
+
+	render(){
+
+		return <Home/>;
+	}
+}
+
+
+export default Qual;
